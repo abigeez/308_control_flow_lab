26d18ed0ac62d7f13165b744af845e6e96dd2e22
Abi Garner <abigail.gee@hotmail.com>
2024-04-12 21:11:47 -0400

    edited some answers

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 
 //prune problem part 1
+//week 1
+const totPlant1 = 20;
+let totPlant2 = 40;
+let totPlant3 = 80;
 
-let totPlant = 20;
-
-if (totPlant < 40) {
-    console.log("Plant!")
-    totPlant++;
-} else if (totPlant > 40 && totPlant < 80) {
-    console.log("Monitor!")
-    totPlant++;
-} else (totPlant > 80)
- {
+if (totPlant1 > 80) {
     console.log("Prune!")
-    totPlant--;
+    
+} else if (totPlant2 > 40 && totPlant2 < 80) {
+    console.log("Monitor!")
+    
+} else 
+{
+    console.log("Plant!")
 }
+////
+// week 2 results
+
+if (totPlant2 < 40) {
+    console.log("Plant!")
+    
+} else if (totPlant2 > 80) {
+    console.log("Prune!")
+    
+} else 
+{
+    console.log("Monitor!")
+}
+
+//////////////// week 3 results 
+if (totPlant3 < 40) {
+    console.log("Plant!")
+    
+} else if (totPlant3 < 80) {
+    console.log("Monitor!")
+    
+} else
+{
+    console.log("Prune!")
+}
+
